59b16c7735364141c51491d11c629e55e116a19c
Iago Rodrigues <iagovendas93@gmail.com>
2025-06-28 05:01:14 +0000

    add scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function home() {
-    return <h1>Leidiane Eu Te amo... s2</h1>
+  return <h1>Leidiane Eu Te amo... s2</h1>;
+}
+
+function Teste() {
+  console.log("indentação errada");
 }
 
 export default home;
